07a4f53db3d9ef1a0b09b58d8a25d67b4c72b8da
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-24 22:36:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,8 +3,8 @@ attr_reader :student, :first_name, :last_name
 end
 
 def initialize(first_name)
-  @first_name = first_name 
-end 
+  @first_name = first_name
+end
 
 def initialize(last_name)
   @last_name = last_name
