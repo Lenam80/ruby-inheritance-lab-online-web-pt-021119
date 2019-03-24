96aaca5642e7d47f979fe8574c769c333860f2d7
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-24 22:40:41 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,5 +1,7 @@
 class Teacher
 attr_reader :teacher
+end
+
 describe "Teacher" do
   let!(:knowledge) { ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"] }
   let!(:teacher) { Teacher.new }
